fcb01ce71fc2c51ff6f57b910b71fe7f0dcf5089
nagira syunya <241004@std.hi-joho.ac.jp>
2025-06-23 12:29:05 +0900

    できました

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        Hero h = new Hero();
+        h.run();
+        SuperHero sh = new SuperHero();
+        sh.run();
+
+        Weapon w = new Weapon();
+    }
 }
--- src/SuperHero.java
+++ src/SuperHero.java
@@ -1,3 +1,38 @@
-public class Hero {
-    
+public class SuperHero extends Hero {
+    boolean flying;
+
+    public SuperHero() {
+        super();
+        System.out.println("Heroのコンストラクタの動作");
+    }
+    public void fly() {
+        this.flying = true;
+        System.out.println("飛び上がった！");
+    }
+    public void land() {
+        this.flying = false;
+        System.out.println("着地した！");
+    }
+    public void run() {
+        System.out.println(this.name + "は撤退した");
+    }
+    public void attack(Matango m) {
+        System.out.println(this.name + "の攻撃！");
+        m.hp -= 5;
+        System.out.println("5ポイントのダメージをあたえた！");
+        if (this.flying) {
+            System.out.println(this.name + "の攻撃！");
+            m.hp -= 5;
+            System.out.println("5ポイントのダメージをあたえた！");
+        }
+        super.attack(m);
+        if (this.flying) {
+            super.attack(m);
+        }
+    }
+    public void test() {
+        super.test();
+        System.out.println("子test");
+    }
 }
+
